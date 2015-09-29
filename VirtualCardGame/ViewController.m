65983bc4cb1abefc5f9e2b90a85ce2b43e3fa36a
Vilson Farias <vilson.farias@arctouch.com>
2015-09-29 19:11:45 -0300

    18 - error

--- VirtualCardGame/ViewController.m
+++ VirtualCardGame/ViewController.m
@@ -5,7 +5,7 @@
 //  Created by Vilson Farias on 5/20/15.
 //  Copyright (c) 2015 ArcTouch. All rights reserved.
 //
-//17
+//18
 
 #import "ViewController.h"
 #import "Deck.h"
@@ -19,7 +19,7 @@
 @property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
 @end
 
-@implementation ViewController
+@implementationViewController
 
 - (PlayingCardGame *) game
 {

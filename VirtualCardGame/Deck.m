0ec5979bf85bc5627d598a34b1ab8581b516f7ef
Vilson Farias <vilson.farias@arctouch.com>
2015-09-30 15:41:15 -0300

    58

--- VirtualCardGame/Deck.m
+++ VirtualCardGame/Deck.m
@@ -1,10 +1,6 @@
 //
 //  Deck.m
-//  VirtualCardGame
-//
-//  Created by Vilson Farias on 5/20/15.
-//  Copyright (c) 2015 ArcTouch. All rights reserved.
-//
+
 
 #import <Foundation/Foundation.h>
 
